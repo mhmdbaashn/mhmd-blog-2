465e3f71787b4e88484180729170793124e3d8e4
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-20 23:07:59 +0000

    Add post model

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def my_blog(request):
-    return HttpResponse('Ana Mohammed')
 
+def my_blog(request):
+    return HttpResponse("Hello Blog!")
--- envy.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://u3uwnizbjvf:RiTMKWu9A4jP@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/talon_sip_feast_23395")
