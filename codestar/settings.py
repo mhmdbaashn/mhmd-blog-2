6b98787ebd9870c50bc10e368a0302fc16646453
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-24 21:48:40 +0000

    Create secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-mhmdbaashn-mhmdblog2-g8hwazcnqtf.ws.codeinstitute-ide.net',
