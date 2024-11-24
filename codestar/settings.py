1aacde82b3290d1e76e969dec721602896eee770
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-24 22:15:20 +0000

    Adds guicorn

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY =  "ggj0c3hid1)n&zgu_febaq0vu8m%my_2941zfi!i!d1ksl5x74"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
